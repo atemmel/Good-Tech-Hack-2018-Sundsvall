a618a1e2384e9a68178b0019f93db39268197896
atemmel <temmeladam@gmail.com>
2018-02-23 15:21:05 +0100

    Added colouring

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+let color_node_fill     = '#444444';
+let color_node_outline  = '#666666';
+let color_node_text     = '#DDDDDD';
+
+let color_binding       = '#666666';
+
 function generateNodes()
 {
   var main = document.getElementById('main');
 
-  var out = '<polyline class="binding" points="200,50 700,100" style="fill:none; stroke:black; stroke-width:4"/>';
+  var out = '<polyline class="binding" points="200,50 700,100" style="fill:none; stroke:' + color_binding + '; stroke-width:4"/>';
 
   out += createNode(200, 50, 40, "Bubbla a");
-  //out += createText(200, 50, "rad 1 jao");
   out += createNode(700, 100, 60, "Bubbla b");
 
   main.innerHTML = out;
@@ -13,11 +18,7 @@ function generateNodes()
 
 function createNode(x, y, radius, string)
 {
-  return '<circle class="node" cx="' + x +'" cy="'+ y + '" r="' + radius + '" stroke="gray" stroke-width="4" fill="white"/>' +
-  '<text x="' + x + '" y="' + y + '" text-anchor="middle" fill="blue" dy="0em"> <tspan x="' + x + '" dy="0.314em">' + string + '</tspan> </text>';
-}
-
-function createText(x, y, string)
-{
-  return '<text x="' + x + '" y="' + y + '" text-anchor="middle" fill="blue" dy="0em"> <tspan x="' + x + '" dy="0.314em">' + string + '</tspan> </text>';
+  return '<circle class="node" cx="' + x +'" cy="'+ y + '" r="' + radius + '" stroke="' + color_node_outline + '" stroke-width="4" fill="' +
+  color_node_fill +'"/>' + '<text x="' + x + '" y="' + y + '" text-anchor="middle" fill="' + color_node_text + '" dy="0em"> <tspan x="' + x  +
+  '" dy="0.314em">' + string + '</tspan> </text>';
 }
