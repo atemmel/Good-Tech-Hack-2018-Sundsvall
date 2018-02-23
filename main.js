e884ef5082ce82add35da1a49894083ceb0bc48b
atemmel <temmeladam@gmail.com>
2018-02-23 14:40:13 +0100

    Lade till mer js

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ function generateNodes()
   var main = document.getElementById('main');
 
   var out = '<polyline class="binding" points="200,50 700,100" style="fill:none; stroke:black; stroke-width:4"/>';
-  out += '<circle class="node" id="nodeA" cx="200" cy="50" r="40" stroke="gray" stroke-width="4" fill="white"/>';
-  out += '<circle class="node" id="nodeB" cx="700" cy="100" r="20" stroke="gray" stroke-width="4" fill="white" />';
+  
+  out += createNode(200, 50, 40);
+  out += createNode(700, 100, 20);
 
   main.innerHTML = out;
+}
+
+function createNode(x, y, radius)
+{
+  return '<circle class="node" cx="' + x +'" cy="'+ y + '" r="' + radius + '" stroke="gray" stroke-width="4" fill="white"/>';
 }
