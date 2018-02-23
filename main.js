d8364d44a4ed0da91798187ffe08ed5c0a4d621c
kjellhard <isakkjellhard@gmail.com>
2018-02-23 19:12:18 +0100

    FTS

--- main.js
+++ main.js
@@ -8,10 +8,35 @@ function generateNodes()
 {
   var main = document.getElementById('main');
 
-  var out = '<polyline class="binding" points="200,50 700,100" style="fill:none; stroke:' + color_binding + '; stroke-width:4"/>';
+  var x = 200;
+  var y = 0;
+  var rad = 80;
+
+  var out ;
+  for (var i = 0; i<6; i++)
+  {
+    x *= (2/3)^(i);
+    y += 50;
+    rad *= (2/3);
+    out += '<polyline class="binding" points="'+ x + ','+ y +' '+ (x+x*(2/3)^(i+1)) + ',' + (y+50) + '" style="fill:none; stroke:' + color_binding + '; stroke-width:' + rad/10 + '"/>';
+    //out += createNode(x, y , rad, "Bubbla " + i);
+  }
+
+  var x = 200;
+  var y = 0;
+  var rad = 80;
+
+  for (var i = 0; i<7; i++)
+  {
+    x *= (2/3)^(i);
+    y += 50;
+    rad *= (2/3);
+    out += createNode(x, y , rad, "Bubbla " + i);
+  }
+  /*var out = '<polyline class="binding" points="200,50 700,100" style="fill:none; stroke:' + color_binding + '; stroke-width:4"/>';
 
   out += createNode(200, 50, 40, "Bubbla a");
-  out += createNode(700, 100, 20, "Bubbla b");
+  out += createNode(700, 100, 20, "Bubbla b");*/
 
   main.innerHTML = out;
 }
