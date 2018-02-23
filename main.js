6c699af0e60477a32260305f19645145ddb58aa0
Alexander Wide <alexanderwide97@gmail.com>
2018-02-23 23:26:30 +0100

    RRAAINNNBOWWW!!!

--- main.js
+++ main.js
@@ -12,55 +12,67 @@ function generateNodes()
   var words = [
     {
         "depth":"0",
+        "group":"Root",
         "name":"Root",
         "children":[
             {
                 "depth":"1",
+                "group":"A",
                 "name":"A"
             },
             {
                 "depth":"1",
+                "group":"B",
                 "name":"B",
                 "children":[
                     {
                         "depth":"2",
+                        "group":"B",
                         "name": "C"
                     },
                     {
                         "depth":"2",
+                        "group":"B",
                         "name": "D",
                         "children":[
                             {
                                 "depth":"3",
+                                "group":"B",
                                 "name": "E"
                             },
                             {
                                 "depth":"3",
+                                "group":"B",
                                 "name": "F"
                             }
                         ]
                     },
                     {
                         "depth":"2",
+                        "group":"B",
                         "name":"G"
                     }
                 ]
             },
             {
                 "depth":"1",
+                "group":"C",
                 "name":"H"
             },
             {
                 "depth":"1",
+                "group":"D",
                 "name":"I",
                 "children":[
                     {
                         "depth":"2",
+                        "group":"D",
                         "name":"J"
                     },
                     {
                         "depth":"2",
-                        "name":"K"
+                        "group":"D",
+                        "name":"J"
                     }
                 ]
             }
@@ -177,22 +189,26 @@ function tick() {
 
 // Color leaf nodes orange, and packages white or blue.
 function color(d) {
-    switch(d.depth){
-
-    case '0':
-      return '#757575';
+    switch(d.group){ 
+            
+    case 'Root':
+      return '#cccccc';
       break;
 
-    case '1':
+    case 'A':
       return '#108bef';
       break;
 
-    case '2':
+    case 'B':
       return '#0fef13';
       break;
 
-    case '3':
+    case 'C':
       return '#ef380f';
+      break;
+            
+    case 'D':
+      return '#c935f2';
       break;
     }
 }
